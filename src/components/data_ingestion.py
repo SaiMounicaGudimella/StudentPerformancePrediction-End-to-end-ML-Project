741c9850c5db4a1fa31cf9a6ab962bb9fdf53fa9
Sai Mounica Gudimella <mounica500110@gmail.com>
2024-08-04 19:37:38 -0400

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,7 +6,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -50,8 +51,8 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
-    #data_transformation=DataTransformation()
-    #train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
     #modeltrainer=ModelTrainer()
     #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
